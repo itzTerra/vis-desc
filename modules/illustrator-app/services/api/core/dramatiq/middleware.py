3ca8396998d1d0709819f47a60dce818ad0fd2d2
xdolezel <xdolezel@fi.muni.cz>
2025-09-08 22:05:37 +0200

    chore: revert preview changes for main

--- modules/illustrator-app/services/api/core/dramatiq/middleware.py
+++ modules/illustrator-app/services/api/core/dramatiq/middleware.py
@@ -1,6 +1,6 @@
 from core.schemas import Evaluator
 
-# from core.tools.evaluators.deberta_mnli import DebertaMNLIEvaluator
+from core.tools.evaluators.deberta_mnli import DebertaMNLIEvaluator
 from dramatiq.middleware import Middleware
 import dramatiq
 from core.tools.evaluators.random_eval import RandomEvaluator
@@ -13,8 +13,8 @@ class WorkerInitializationMiddleware(Middleware):
     def before_worker_boot(self, broker, worker: dramatiq.Worker):
         worker_resources["redis"] = get_redis_client()
         worker_resources["evaluators"] = {
-            # Evaluator.deberta_mnli: DebertaMNLIEvaluator(create_model=True),
-            Evaluator.random: RandomEvaluator()
+            Evaluator.deberta_mnli: DebertaMNLIEvaluator(create_model=True),
+            Evaluator.random: RandomEvaluator(),
         }
 
         worker.logger.info("Worker initialized resources!")

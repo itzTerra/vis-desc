d09f463c1617a89a2c9fb4758268d90b2c04ac5a
xdolezel <xdolezel@fi.muni.cz>
2025-09-08 15:34:49 +0200

    build: remove default sqlite db

--- modules/illustrator-app/services/api/api/urls.py
+++ modules/illustrator-app/services/api/api/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,6 +27,6 @@ websocket_urlpatterns = [
 ]
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("api/", api.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
